8ba2d09cb350fe11c4fedd29fec77d0fd82de91c
EsraSham <eaa23eas@students.eaaa.dk>
2024-10-08 20:43:05 +0200

    j
    jj

---
+++ js/medlem.js
@@ -0,0 +1,9 @@
+function showAlert() {
+        document.getElementById("notifikation").style.display = "block"; // Vis popup
+        return false; // Forhindrer formularen i at blive indsendt
+    }
+
+    function closePopup() {
+        document.getElementById("notifikation").style.display = "none"; // Skjul popup
+    }
+
